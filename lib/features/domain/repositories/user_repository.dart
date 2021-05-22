60682fa2790eb8acc0dad304190baae99eb51897
Luis Eduardo <Eduardo2125LES>
2021-05-22 18:09:31 -0500

    create repository

---
+++ lib/features/domain/repositories/user_repository.dart
@@ -0,0 +1,13 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:placeholder_cleanarchitecture/core/error/failures.dart';
+import 'package:placeholder_cleanarchitecture/features/domain/entities/user_entity.dart';
+
+
+
+
+abstract class UserRepository {
+
+  Future<Either<Failure, List <UserEntity>>> getUsers();
+
+}
