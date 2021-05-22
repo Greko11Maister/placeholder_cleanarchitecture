084aee7bc02cc46eea0d9cf59597a17a153536e8
Gregory Iscala <greko11maister@gmail.com>
2021-05-22 17:06:38 -0500

    Change structure of project

---
+++ lib/features/domain/entities/user_entity.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+class UserEntity  extends Equatable {
+
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  AddressEntity? address;
+  String? phone;
+  String? website;
+  CompanyEntity? company;
+
+  UserEntity({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.address,
+    this.phone,
+    this.website,
+    this.company,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    username,
+    email,
+    address,
+    phone,
+    website,
+    company,
+  ];
+
+
+}
